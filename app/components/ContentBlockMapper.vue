8eb1512579e863cd95823d4801f3e79361d68772
Martijn Halekor <martijn@cardan.com>
2025-06-25 18:05:54 +0200

    improve performance for content block mapper

--- app/components/ContentBlockMapper.vue
+++ app/components/ContentBlockMapper.vue
@@ -8,100 +8,47 @@ const props = defineProps<{ content?: StatamicContentBlock[] }>();
       <div v-if="contentBlock.type === 'text'" v-html="contentBlock.text" />
 
       <StatamicIcon
-        v-if="contentBlock?.type === 'icon'"
+        v-else-if="contentBlock?.type === 'icon'"
         :icon="contentBlock.icon"
         class="statamic-icon"
       />
 
       <StatamicIconList
-        v-if="contentBlock?.type === 'list'"
+        v-else-if="contentBlock?.type === 'list'"
         :data="contentBlock.list"
       />
 
       <StatamicImage
-        v-if="contentBlock?.type === 'image'"
+        v-else-if="contentBlock?.type === 'image'"
         :data="contentBlock.image"
         :caption="contentBlock.image_caption"
       />
 
       <StatamicButton
-        v-if="contentBlock?.type === 'button'"
+        v-else-if="contentBlock?.type === 'button'"
         :data="contentBlock"
       />
 
       <StatamicButtonGroup
-        v-if="contentBlock?.type === 'button_group'"
+        v-else-if="contentBlock?.type === 'button_group'"
         :data="contentBlock"
       />
 
       <StatamicQuote
-        v-if="contentBlock?.type === 'quote'"
+        v-else-if="contentBlock?.type === 'quote'"
         :data="contentBlock"
       />
 
       <LazyStatamicForm
-        v-if="contentBlock?.type === 'form'"
+        v-else-if="contentBlock?.type === 'form'"
         :data="contentBlock"
       />
 
       <VideoPlayer
-        v-if="contentBlock?.type === 'video'"
+        v-else-if="contentBlock?.type === 'video'"
         :url="contentBlock?.video_url"
         :caption="contentBlock?.image_caption"
       />
     </template>
   </div>
 </template>
-
-<style>
-.bard-content {
-  margin-inline: auto;
-  display: grid;
-  gap: 1rem;
-
-  > :first-child {
-    margin-block: 0;
-  }
-
-  .statamic-icon svg {
-    width: var(--font-size-xxl);
-    height: var(--font-size-xxl);
-    margin-block-end: 1rem;
-  }
-
-  figure {
-    margin-block-end: 2rem;
-    border-radius: var(--radius-sm);
-  }
-
-  p,
-  ul,
-  ol {
-    > a {
-      &:hover {
-        color: var(--color-accent-text);
-      }
-    }
-
-    &:has(+ h2) {
-      margin-block-end: 3rem;
-    }
-    &:has(+ h3) {
-      margin-block-end: 2.5rem;
-    }
-    &:has(+ h4) {
-      margin-block-end: 2rem;
-    }
-    &:has(+ h5) {
-      margin-block-end: 1.5rem;
-    }
-    &:has(+ h6) {
-      margin-block-end: 1rem;
-    }
-  }
-
-  ul > li::marker {
-    color: var(--color-accent-graphic);
-  }
-}
-</style>
