6b66aac9a285550a30025da6652bace870d65081
Martijn Halekor <martijn@cardan.com>
2025-12-20 00:57:13 +0100

    remove cache key from preview route

--- app/pages/preview.vue
+++ app/pages/preview.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
 const route = useRoute();
 
-const { data: entry } = await useAsyncData<{ data: StatamicPageEntry }>(
-  `${route.path}-${route.query.token}`,
-  () =>
-    $fetch('/api/entry/preview/', {
-      baseURL: useRuntimeConfig().public.statamicUrl,
-      query: {
-        id: route.query.id,
-        url: route.query.url,
-        token: route.query.token,
-        preview: true,
-      },
-    })
+const { data: entry } = await useAsyncData<{ data: StatamicPageEntry }>(() =>
+  $fetch('/api/entry/preview/', {
+    baseURL: useRuntimeConfig().public.statamicUrl,
+    query: {
+      id: route.query.id,
+      url: route.query.url,
+      token: route.query.token,
+      preview: true,
+    },
+  })
 );
 const page = computed(() => entry.value?.data);
 console.log(page.value);
